85bc455352eac95dff33ade9d4fc818143ab43e6
Promise <promiseprince01@gmail.com>
2021-04-02 03:07:16 +0100

    Added the return function on line 23 app.js file

--- tracaproperty/app.js
+++ tracaproperty/app.js
@@ -2,9 +2,37 @@
 
 // Item Controller 
 const ItemCtrl = (function() {
-  console.log('yes it works');
+  const Item = function(id, name, price){
+    this.id = id;
+    this.name = name;
+    this.price = price;
+  }
+
+  //Data Structure
+  const data = {
+    items: [
+      {id: 0, name: 'Samsung Electronics', price:N50000},
+      {id: 1, name: 'Dinning Table', price:N100000},
+      {id: 2, name: 'Wallpapers', price:N80000},
+    ],
+    currentitem: null,
+    totalPrice: 0
+  }
+
+  // Return the data 
+  return {
+    logData: function() {
+      return data;
+    }
+  }
 })();
 
 // UI Controller 
+const UICtrl = (function() {
+  
+})();
 
 // App Controller 
+const App = (function(ItemCtrl, UiCtrl) {
+  
+})();
